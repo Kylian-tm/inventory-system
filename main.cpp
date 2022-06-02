233fe00367331d163e60b73af5e1f85da10ee072
kylian timmers <56076746+Kylian-tm@users.noreply.github.com>
2022-06-02 09:40:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
 
 int main(){
-  
+    sf::RenderWindow window(sf::VideoMode(800,800), "inventory-system"); 
+    while(window.isOpen()){
+          sf::Event event;
+            while (window.pollEvent(event))
+            {
+                switch (event.type)
+                {
+                    case sf::Event::Closed:
+                        window.close();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            window.clear();
+            window.display();
+            std::this_thread::sleep_for(0.08s);
+        }
+    }
 } 
